0e2f5cb9a77d711f4d54ae52e81a8c368952918f
Jimmy Loloy <loloy.laurencio@gmail.com>
2022-07-22 09:48:40 -0500

    ADD: Navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+export default function Navbar() {
+  return (
+    <div className="header-wrapper">
+      <header className="header">
+        <nav className="nav">
+          <ul className="nav-list">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                All
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Me
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="header-logo">
+          <a className="logo-link" href="/index.html">
+            <img
+              className="logo-img"
+              src="./public/assets/logo.svg"
+              alt="bootcamp logo"
+            />
+            <h2 className="logo-text">My Daily Bootcamp</h2>
+          </a>
+        </div>
+      </header>
+    </div>
+  );
+}
