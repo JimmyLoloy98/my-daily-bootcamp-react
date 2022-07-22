4d9757a04475400711552f35c0b84b16305fb8aa
Jimmy Loloy <loloy.laurencio@gmail.com>
2022-07-22 11:20:22 -0500

    ADD: structure update

--- src/components/Storie.jsx
+++ src/components/Storie.jsx
@@ -1,17 +1,48 @@
 export default function Storie() {
   return (
-    <a class="link-stories" href="#">
-      <li class="frame-storie">
-        <div class="img-storie1 img-storie">
-          <img
-            class="profile-photo"
-            src="../../public/assets/img/stories/profile 1.jpg"
-            alt="foto de perfil de un usuario"
-          />
-          <h3 class="profile-name">Kierra Gentry</h3>
-        </div>
-        <div class="storie-borde"></div>
-      </li>
-    </a>
+    <div className="stories">
+      <h2 className="tittle-stories">Stories</h2>
+      <ul className="stories-sqare">
+        <a className="link-stories" href="#">
+          <li className="frame-storie">
+            <div className="img-storie1 img-storie">
+              <img
+                className="profile-photo"
+                src="../../public/assets/img/stories/profile 1.jpg"
+                alt="foto de perfil de un usuario"
+              />
+              <h3 className="profile-name">Kierra Gentry</h3>
+            </div>
+            <div className="storie-borde"></div>
+          </li>
+        </a>
+        <a className="link-stories" href="#">
+          <li className="frame-storie">
+            <div className="img-storie2 img-storie">
+              <img
+                className="profile-photo"
+                src="../../public/assets/img/stories/profile 2.jpg"
+                alt="foto de perfil de un usuario"
+              />
+              <h3 className="profile-name">Bradyn Krammer</h3>
+            </div>
+            <div className="storie-borde"></div>
+          </li>
+        </a>
+        <a className="link-stories" href="#">
+          <li className="frame-storie">
+            <div className="img-storie3 img-storie">
+              <img
+                className="profile-photo"
+                src="../../public/assets/img/stories/profile 3.jpg"
+                alt="foto de perfil de un usuario"
+              />
+              <h3 className="profile-name">Pierre Cox</h3>
+            </div>
+            <div className="storie-borde"></div>
+          </li>
+        </a>
+      </ul>
+    </div>
   );
 }
