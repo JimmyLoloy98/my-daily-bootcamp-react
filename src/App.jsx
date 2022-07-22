33124fb2efab898fd18896a8a452a2183a953afb
Pavel Leonardo Mansilla Goyas <pavel.mansilla@gmail.com>
2022-07-22 11:18:47 -0500

    team section added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import SidebarLeft from "./components/SidebarLeft";
 import DailyContent from "./components/DailyContent";
 import SidebarRigth from "./components/SidebarRigth";
 import ProfileCard from "./components/ProfileCard";
+import Team from "./components/Teams";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Main>
         <SidebarLeft>
           <ProfileCard></ProfileCard>
+          <Team></Team>
         </SidebarLeft>
         <DailyContent></DailyContent>
         <SidebarRigth></SidebarRigth>
