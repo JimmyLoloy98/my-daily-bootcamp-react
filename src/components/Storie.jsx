112d0753b21e68040dd5068a614168ef9ef37b30
Jimmy Loloy <loloy.laurencio@gmail.com>
2022-07-22 10:43:11 -0500

    ADD: html structure added

---
+++ src/components/Storie.jsx
@@ -0,0 +1,17 @@
+export default function Storie() {
+  return (
+    <a class="link-stories" href="#">
+      <li class="frame-storie">
+        <div class="img-storie1 img-storie">
+          <img
+            class="profile-photo"
+            src="../../public/assets/img/stories/profile 1.jpg"
+            alt="foto de perfil de un usuario"
+          />
+          <h3 class="profile-name">Kierra Gentry</h3>
+        </div>
+        <div class="storie-borde"></div>
+      </li>
+    </a>
+  );
+}
