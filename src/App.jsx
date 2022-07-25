b34d2f8178983908dccf3d9714f548f4402f7f6d
Jimmy Loloy <loloy.laurencio@gmail.com>
2022-07-25 09:52:18 -0500

    ADD: open-close modal function

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,21 @@ import SidebarRigth from "./components/SidebarRigth";
 import ProfileCard from "./components/ProfileCard";
 import Team from "./components/Teams";
 import FollowUp from "./components/FollowUp";
-import NewPost from "./components/NewPost";
+import InputNewPost from "./components/InputNewPost";
 import Post from "./components/Post";
+import Footer from "./components/Footer";
+import ModalNewPost from "./components/ModalNewPost";
+import { useState } from "react";
 
 function App() {
+  const [stateModal, setStateModal] = useState(false);
+
+  let user = fetch(
+    "https://my-daily-bootcamp.herokuapp.com/users/37.json"
+  ).then((response) => {
+    console.log(response);
+  });
+
   return (
     <>
       <Navbar />
@@ -23,14 +34,16 @@ function App() {
         </SidebarLeft>
 
         <DailyContent>
-          <NewPost />
+          <InputNewPost newPost={setStateModal} />
           <Post />
+          <ModalNewPost open={stateModal} setOpen={setStateModal} />
         </DailyContent>
 
         <SidebarRigth>
           <Storie />
           <Ads />
           <FollowUp />
+          <Footer />
         </SidebarRigth>
       </Main>
     </>
