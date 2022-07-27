b0231bc2a38568a2da21cee7b933561f42ba1e2f
Jimmy Loloy <loloy.laurencio@gmail.com>
2022-07-27 09:11:53 -0500

    ADD: dropdown menu has added

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,21 +3,30 @@ import trash_icon from "../assets/img/trash.svg";
 import profile_img from "../assets/img/profile.jpg";
 
 import DataContext from "../context/context";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 export default function Post() {
   let dataContext = useContext(DataContext);
+  const [options, setOptions] = useState(false);
+  
+  let buttonActionPost = () => {
+    options ? setOptions(false) : setOptions(true);
+  };
 
   return (
     <>
       {dataContext.posts.map((post, index) => {
         return (
           <div className="public" key={index}>
-            <div className="pop-container">
-              <button className="dropdown-button">
+            <div
+              className="pop-container"
+            >
+              <button className="dropdown-button" onClick={buttonActionPost}>
                 <img src={dots_icon} alt="Menú de Opciones" />
               </button>
-              <div className="popup-container">
+              <div
+                className={options ? "popup-container" : "popup-container hidden"}
+              >
                 <button className="pop-up">
                   <img src={trash_icon} />
                   <p>Delete</p>
