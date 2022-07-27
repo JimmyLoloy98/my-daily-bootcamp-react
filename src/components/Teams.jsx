dcdd71c3d463a47a25d1df2cf7cb9e344b38271b
Jimmy Loloy <loloy.laurencio@gmail.com>
2022-07-27 17:32:43 -0500

    ADD: show data in posts

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -1,7 +1,3 @@
-import team1 from "../assets/img/team/team-1.jpg";
-import team2 from "../assets/img/team/team-2.jpg";
-import team3 from "../assets/img/team/team-3.jpg";
-import team4 from "../assets/img/team/team-4.jpg";
 import { useContext } from "react";
 import DataContext from "../context/context";
 
@@ -16,7 +12,7 @@ const Team = () => {
           if (index > 1 && index < 5) {
             return (
               <div className="date-teams1" key={index}>
-                <a className="link-team" href={teammate.twitter_url}>
+                <a className="link-team" href={teammate.twitter_url} target="_blank">
                   <img
                     className="team-img"
                     src={teammate.profile_url}
@@ -28,25 +24,6 @@ const Team = () => {
             );
           }
         })}
-
-        {/* <div className="date-teams1">
-          <a className="link-team" href="#">
-            <img className="team-img" src={team1} alt="Alden Cantrell" />
-            <p className="text-teams">Alden Cantrell</p>
-          </a>
-        </div>
-        <div className="date-teams">
-          <a className="link-team" href="#">
-            <img className="team-img" src={team3} alt="Miranda Shaffer" />
-            <p>Miranda Shaffer</p>
-          </a>
-        </div>
-        <div className="date-teams2">
-          <a className="link-team" href="#">
-            <img className="team-img" src={team4} alt="Alvaro Mcgee" />
-            <p>Alvaro Mcgee</p>
-          </a>
-        </div> */}
       </div>
     </div>
   );
